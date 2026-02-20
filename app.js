33bef9a28a2a11c3a9a523abb5b7bd62a7fe74bf
37975 <theejuthakonsombat@gmail.com>
2026-02-20 15:27:25 +0700

    COMPLETE - ONE WAY TRANMISSION

---
+++ app.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCoU3257zviNl_L0EyLSRHhW5bmXsnK_sk",
+  authDomain: "embedded-cc90e.firebaseapp.com",
+  databaseURL: "https://embedded-cc90e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "embedded-cc90e",
+  storageBucket: "embedded-cc90e.firebasestorage.app",
+  messagingSenderId: "24395871863",
+  appId: "1:24395871863:web:47b3d1ccda03a2b9947872",
+  measurementId: "G-Y3Q3377X75"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
